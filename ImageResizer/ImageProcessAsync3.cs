9ff8c185c00fd6f5f1205d71f6181f5a0640e484
devilmiter <devilmiter1999@yahoo.com.tw>
2019-09-18 15:48:34 +0800

    1
     20190917 Miter
    1. 完成非同步5個版本

---
+++ ImageResizer/ImageProcessAsync3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    public class ImageProcessAsync3 : ImageProcess
+    {
+        public override string DestinationPath { get { return Path.Combine(Environment.CurrentDirectory, "ASyncOutput3"); } }
+
+        public override string Name { get { return "非同步3(Parallel版本)"; } }
+
+        /// <summary>
+        /// 進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        protected override void ResizeImages(string sourcePath, string destPath, double scale)
+        {
+            var allFiles = this.FindImages(sourcePath);
+            Parallel.ForEach(allFiles, filePath =>
+            {
+                this.ResizeImageWork(filePath, destPath, scale);
+            });
+        }
+
+        private void ResizeImageWork(string filePath, string destPath, double scale)
+        {
+            Image imgPhoto = Image.FromFile(filePath);
+            string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+
+            int destionatonWidth = (int)(sourceWidth * scale);
+            int destionatonHeight = (int)(sourceHeight * scale);
+
+            Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
+                sourceWidth, sourceHeight,
+                destionatonWidth, destionatonHeight);
+
+            string destFile = Path.Combine(destPath, imgName + ".jpg");
+            processedImage.Save(destFile, ImageFormat.Jpeg);
+        }
+    }
+}
---
+++ ImageResizer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ImageProcessList = new List<ImageProcess>();
+            ImageProcessList.Add(new ImageProcess());
+            ImageProcessList.Add(new ImageProcessAsync());
+            ImageProcessList.Add(new ImageProcessAsync2());
+            ImageProcessList.Add(new ImageProcessAsync3());
+            ImageProcessList.Add(new ImageProcessAsync4());
+            ImageProcessList.Add(new ImageProcessAsync5());
+
+            long firstTime = -1;
+            ImageProcessList.ForEach(imageProcess =>
+            {
+                imageProcess.Run();
+                if(firstTime == -1)
+                {
+                    firstTime = imageProcess.RunningTime;
+                }
+                else
+                {
+                    long secondTime = imageProcess.RunningTime;
+                    Console.WriteLine($"節省約：{((firstTime - secondTime) / Convert.ToDouble(firstTime)).ToString("#0.00%")}");
+                }
+            });
+
+            Console.Write("--程式已結束--");
+            Console.ReadLine();
+        }
+    }
+}
